e74772da343c3cefd1db2c41d5a78ddaa1c647c6
samo <janezic.orion@gmail.com>
2023-05-11 18:09:01 +0200

    Add events on Application.php

--- Application.php
+++ Application.php
@@ -6,8 +6,12 @@ use samojanezic\phpmvc\db\Database;
 
 class Application
 {
-	public static string $ROOT_DIR;
+	const EVENT_BEFORE_REQUEST = 'beforeRequest';
+	const EVENT_AFTER_REQUEST = 'afterRequest';
 
+	protected array $eventListeners = [];
+
+	public static string $ROOT_DIR;
 	public string $layout = 'main';
 	public string $userClass;
 	public Router $router;
@@ -43,6 +47,7 @@ class Application
 	}
 	public function run()
 	{
+		$this->triggerEvent(self::EVENT_BEFORE_REQUEST);
 		try {
 			echo $this->router->resolve();
 		} catch(\Exception $e) {
@@ -82,5 +87,18 @@ class Application
 	public static function isGuest()
 	{
 		return !self::$app->user;
+	}
+
+	public function triggerEvent($eventName)
+	{
+		$callbacks = $this->eventListeners[$eventName] ?? [];
+		foreach ($callbacks as $callback) {
+			call_user_func($callback);
+		}
+	}
+
+	public function on($eventName, $callback)
+	{
+		$this->eventListeners[$eventName][] = $callback;
 	}
 }
